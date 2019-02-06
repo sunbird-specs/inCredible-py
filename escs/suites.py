b177ec4c624bb6970e7b844cb4da99463299108a
Shailesh <kochhar@users.noreply.github.com>
2019-02-06 19:32:12 +0530

    Fixed import.

--- escs/suites.py
+++ escs/suites.py
@@ -5,6 +5,8 @@ module implementing the rsa_signature_2018 signature suite and others
 """
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
 from pyld import jsonld
 
 class SignatureSuite(object):
