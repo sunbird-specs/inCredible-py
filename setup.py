0bcbe1993f6d73022add5f8f07ec2135343073b4
Shailesh <kochhar@users.noreply.github.com>
2019-02-18 20:34:17 +0530

    Added normalisation as script.

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from distutils.core import setup
 setup(name='skillcreds',
       version='0.1',
       description='Electronic Skills Credential Specification Utilities',
-      url='https://bharatskills.gov.in/skills-credentials',
+      url='https://skillcredentialspec.org/skills-credentials',
       packages=['skillcreds'],
       requires=[
         "cryptography (>=2.5, <2.6)",
         "PyLD (>=1.0.4, <1.1)",
         "requests (>=2.21.0, <2.22)",
       ],
-      scripts=['scripts/sign.py'])
+      scripts=[
+        'scripts/sign.py',
+        'scripts/normalize.py'
+      ])
